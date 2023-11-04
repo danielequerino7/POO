272f8c90bee4eeb76d41089e5d43a99ff6e8a6dd
Daniele Querino <128300815+danielequerino7@users.noreply.github.com>
2023-11-04 15:28:58 -0300

    fazendo alterações na classe Principal.java do arquivo Q1L1_ProjCachorro

--- Lista 1/Q1L1_ProjCachorro/src/visao/Principal.java
+++ Lista 1/Q1L1_ProjCachorro/src/visao/Principal.java
@@ -9,11 +9,7 @@ public class Principal {
 		// A) objeto ja inicializado
 		Cachorro C1; // declarando uma variavel ponteiro 
 		C1 = new Cachorro("cachorro sem nome", "nenhuma raça definida", 0); // instanciando o objeto e referenciando no ponteiro 
-		
-		//System.out.println("Nome do cachorro: " + C1.getNome());
-		//System.out.println("Raça do cachorro: " + C1.getRaca());
-		//System.out.println("Idade do cachorro: " + C1.getIdade());
-		
+	
 		// B)
 		System.out.println("INFORMANDO OS ATRIBUTOS USANDO O MÉTODO GET");
 		System.out.println("Nome: " + C1.getNome());
@@ -24,10 +20,6 @@ public class Principal {
 		C1.setNome("Bolinha");
 		C1.setRaca("Rottweiler");
 		C1.setIdade(10);
-		/*System.out.println("\nEXIBINDO OS ESTADOS APÓS O MÉTODO SET\n");
-		System.out.println("Nome do cão: " + C1.getNome());
-		System.out.println("Raça do cão: " + C1.getRaca());
-		System.out.println("O cão tem " + C1.getIdade() + " anos.");*/
 		
 		// D)
 		System.out.println("\nUSANDO O MÉTODO INFORMAR ESTADO");
@@ -36,17 +28,19 @@ public class Principal {
 		// E)
 		Cachorro C2 = C1.clonarCachorro(); // clonando o objeto C1
 		System.out.println("\nVERIFICANDO A IGUALDADE DOS ESTADOS DO CLONE E DO OBJETO");
-		if(C1.equals(C2)) {
+		if(C1.informarEstado().equals(C2.informarEstado())) 
 			System.out.println("Os estados são iguais");	
-		}
-		else {
+		else 
 			System.out.println("Os estados não são iguais");
-		}
 		
 		// F)
 		System.out.println("\nESTADOS DO CLONE DO CACHORRO C1 (C2)");
-		System.out.println(C2.informarEstado());
+		System.out.println(C2.informarEstado()+"\n");
+		
+		if(C1.equals(C2)) 
+			System.out.println("Os estados são iguais");	
+		else 
+			System.out.println("Os estados não são iguais");
 		
 	}
-
 }
